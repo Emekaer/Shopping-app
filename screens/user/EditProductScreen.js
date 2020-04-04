945a8693454237af928067f936846081d84e2e1e
ER <emekaokaforer@gmail.com>
2020-04-04 23:12:07 +0100

    Updated

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, StyleSheet, TextInput, ScrollView, } from "react-native";
+import { View, Text, StyleSheet, TextInput, ScrollView, Alert} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import * as productAction from "../../store/actions/products";
 
-const EditProductScreen = props => {
+const EditProductScreen = (props) => {
   const { productId } = props.route.params;
   dispatch = useDispatch();
-  const editedProduct = useSelector(state =>
-    state.products.userProducts.find(prod => prod.id === productId)
+  const editedProduct = useSelector((state) =>
+    state.products.userProducts.find((prod) => prod.id === productId)
   );
 
   const [title, setTitle] = useState(editedProduct ? editedProduct.title : "");
+  const [titleIsValid, setTitleIsValid] = useState("false");
   const [imageUrl, setImageUrl] = useState(
     editedProduct ? editedProduct.imageUrl : ""
   );
@@ -20,21 +21,36 @@ const EditProductScreen = props => {
   );
 
   const submitHandler = useCallback(() => {
+    if(!titleIsValid){
+      Alert.alert('Wrong input!', 'Please Check for errors in the form.',[{
+        text: 'Okay'
+      }]);
+      return;
+    }
     if (editedProduct) {
       dispatch(
         productAction.updateProduct(productId, title, description, imageUrl)
-       );
+      );
     } else {
-     dispatch(
+      dispatch(
         productAction.createProduct(title, description, imageUrl, +price)
       );
-   };
+    }
     props.navigation.goBack();
   }, [dispatch, productId, title, description, imageUrl, price]);
 
   useEffect(() => {
     props.navigation.setParams({ submit: submitHandler });
   }, [submitHandler]);
+
+  const titleChangeHandler = (text) => {
+    if (text.trim().length === 0) {
+      setTitleIsValid(false);
+    } else {
+      setTitleIsValid(true);
+    }
+    setTitle(text);
+  };
 
   return (
     <ScrollView>
@@ -44,15 +60,21 @@ const EditProductScreen = props => {
           <TextInput
             style={styles.input}
             value={title}
-            onChangeText={text => setTitle(text)}
+            onChangeText={titleChangeHandler}
+            keyboardType="default"
+            autoCapitalize="sentences"
+            autoCorrect
+            returnKeyType="next"
           />
+          {!titleIsValid && <Text>Please enter valid title!</Text>}
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>imgeUrl</Text>
           <TextInput
             style={styles.input}
             value={imageUrl}
-            onChangeText={text => setImageUrl(text)}
+            onChangeText={(text) => setImageUrl(text)}
+            returnKeyType="next"
           />
         </View>
         {editedProduct ? null : (
@@ -61,7 +83,9 @@ const EditProductScreen = props => {
             <TextInput
               style={styles.input}
               value={price}
-              onChangeText={text => setPrice(text)}
+              onChangeText={(text) => setPrice(text)}
+              keyboardType="decimal-pad"
+              returnKeyType="next"
             />
           </View>
         )}
@@ -70,7 +94,9 @@ const EditProductScreen = props => {
           <TextInput
             style={styles.input}
             value={description}
-            onChangeText={text => setDescription(text)}
+            onChangeText={(text) => setDescription(text)}
+            returnKeyType="next"
+            onSubmitEditing={submitHandler}
           />
         </View>
       </View>
@@ -80,21 +106,21 @@ const EditProductScreen = props => {
 
 const styles = StyleSheet.create({
   form: {
-    margin: 20
+    margin: 20,
   },
   formControl: {
-    width: "100%"
+    width: "100%",
   },
   label: {
     fontFamily: "openSansBold",
-    marginVertical: 8
+    marginVertical: 8,
   },
   input: {
     paddingHorizontal: 2,
     paddingVertical: 5,
     borderBottomColor: "#ccc",
-    borderBottomWidth: 1
-  }
+    borderBottomWidth: 1,
+  },
 });
 
 export default EditProductScreen;
